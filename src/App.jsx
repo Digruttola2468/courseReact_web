f6db141d4c59e1f536818fc4eebc1a6078d58c55
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-11 11:35:58 -0300

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Hola mundo</h1>
+  return <ul>
+    <li><a href="">Enlace 1</a></li>
+    <li><a href="">Enlace 2</a></li>
+    <li><a href="">Enlace 3</a></li>
+    <li><a href="">Enlace 4</a></li>
+  </ul>
 }
 
 export default App;
