dfb77b933c11e041aaeab825346f0a0636c3a72e
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-13 14:27:12 -0300

    Separar Componentes

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,17 +3,26 @@ import {useState} from "react";
 export default function TaskForm({createTask}) {
 
     const [title, setTitle] = useState("")
+    const [descripcion, setDescripcion] = useState("")
 
     const handleSubmit = (e )=> {
         e.preventDefault();
         
-        createTask(title);
+        createTask({
+          title,descripcion
+        });
+
+        setTitle('');
+        setDescripcion('');
     }
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Escribe Tu Tarea" 
         onChange={e => setTitle(e.target.value)}
+        value={title}
+        autoFocus
       />
+      <textarea name="" id="" placeholder="Descripcion" cols="20" rows="5" onChange={e => setDescripcion(e.target.value)} value={descripcion}></textarea>
       <button>Guardar</button>
     </form>
   );
